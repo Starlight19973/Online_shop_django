35e488594ab3a57f41f02ddcdc7483681dbe1109
Starlight <starlight1997@yandex.ru>
2024-01-30 19:56:50 +0700

    'register'

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from app.settings import DEBUG
+from app import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,7 +11,9 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog'))
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls"))
+
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -4,11 +4,11 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - главная',
         'content': 'Магазин мебели - Home',
-        'categories': categories
+
 
     }
     return render(request, 'main/index.html', context)
