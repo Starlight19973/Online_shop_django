dee8b81ef5e7c98927a7db0004fd3029f23e2ebe
Starlight <starlight1997@yandex.ru>
2024-01-31 15:40:38 +0700

    'slugs'

--- goods/admin.py
+++ goods/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 from .models import Categories, Products
 
-
-#admin.site.register(Categories)
-
-
-#admin.site.register(Products)
-
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
--- goods/views.py
+++ goods/views.py
@@ -12,8 +12,11 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request, product_id):
-    product = Products.objects.get(id=product_id)
+def product(request, product_slug=False, product_id=False):
+    if product_id:
+        product = Products.objects.get(id=product_id)
+    else:
+        product = Products.objects.get(slug=product_slug)
     context = {
         'product': product
     }
