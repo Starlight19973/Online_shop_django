42db1a0229b830fee02f7583721867bccb30a989
Starlight <starlight1997@yandex.ru>
2024-01-31 15:41:24 +0700

    'slugs_2'

--- goods/views.py
+++ goods/views.py
@@ -12,11 +12,8 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request, product_slug=False, product_id=False):
-    if product_id:
-        product = Products.objects.get(id=product_id)
-    else:
-        product = Products.objects.get(slug=product_slug)
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
     context = {
         'product': product
     }
